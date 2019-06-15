b3126f70d8575ca17b6b225e608ae69e36ef1b0c
Andrew Wardrobe <andrew.g.wardrobe@googlemail.com>
2019-06-15 18:44:38 +0100

    different test on exec example

--- spec/integration/example_spec.rb
+++ spec/integration/example_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 require 'serverspec_launcher/spec_helper'
 
-describe package('openssh-server') do
-  it { should be_installed }
+describe file '/etc/hosts' do
+  it { should exist }
+  it { should be_a_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
 end
